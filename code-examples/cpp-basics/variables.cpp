0ecb85708f217b78467778cdc09ba0517b0f7da7
vivsvsm <vivsvsm@zohomail.in>
2025-10-07 15:16:33 +0530

    Practice: C++ Hello World and variables

---
+++ code-examples/cpp-basics/variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    double height = 5.9;
+    string name = "PhD Researcher";
+    
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " feet" << endl;
+    
+    return 0;
+}
